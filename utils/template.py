a40d5b3fd08bbc93d8cc5a9b2eb643a286dfdc0d
truongthuanr <truongthuan.r@gmail.com>
2025-04-22 18:27:15 +0700

    custom render template response

---
+++ utils/template.py
@@ -0,0 +1,17 @@
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+# Khởi tạo templates
+templates = Jinja2Templates(directory="templates")
+
+def render_template(request: Request, template_name: str, context: dict = {}) -> HTMLResponse:
+    # Lấy user từ middleware
+    user = getattr(request.state, "user", None)
+    # Merge context
+    context = {
+        **context,
+        "request": request,
+        "user": user
+    }
+    return templates.TemplateResponse(template_name, context)
